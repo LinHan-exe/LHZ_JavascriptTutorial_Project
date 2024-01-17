d12a7d48f27693fd43adbf37916c3687544a9514
= <linhanzhou.nyc@gmail.com>
2024-01-17 15:29:55 -0500

    finished

--- script.js
+++ script.js
@@ -1,1 +1,25 @@
-//javascript
+//javascript examples
+console.log("hello world");
+
+//variables
+x = 3; y = 5; z = x+y; example = "hello"; console.log(z);
+
+
+//conditionals
+if(x+y == 8){
+    console.log("yes!");
+}
+
+//arrays
+const names = ["bob", "joe", "oppenheimer"];
+console.log(names[0]);
+
+
+//functions
+function helloworld(word1, word2){
+    console.log(word1 + " " + word2);
+}
+helloworld("hello", "world");
+
+
+
